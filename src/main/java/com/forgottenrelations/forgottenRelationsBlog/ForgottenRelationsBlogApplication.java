be4f9d17c01928f48edc3e5b676650db03c33b33
forgottenrelations <forgottenrelations@gmail.com>
2021-04-28 09:34:14 -0500

    first commit

---
+++ src/main/java/com/forgottenrelations/forgottenRelationsBlog/ForgottenRelationsBlogApplication.java
@@ -0,0 +1,13 @@
+package com.forgottenrelations.forgottenRelationsBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgottenRelationsBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgottenRelationsBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forgottenrelations/forgottenRelationsBlog/ForgottenRelationsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forgottenrelations.forgottenRelationsBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgottenRelationsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
